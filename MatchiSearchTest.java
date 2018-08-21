388d982fde43e1950650b2681d7bfd234c1b2403
Lotta Berg <lottaberg113@gmail.com>
2018-08-21 13:25:36 +0200

    Test case code for Login and Swish payment

--- MatchiSearchTest.java
+++ MatchiSearchTest.java
@@ -4,37 +4,46 @@ import org.junit.Test;
 
 
 public class MatchiSearchTest {
-	/**
-	 * Test order method created in the bottom
-	 */
+	
 
 //	@Test
 	public void LogInTest() {
 		MatchiSearch matchi = new MatchiSearch();
 		matchi.openSite("https://beta1.matchi.se/?lang=sv");
 		matchi.logIn();
-		matchi.book();
-		matchi.searchInOutCourt();
-		matchi.chooseInOutCourt("Inomhus");
-		matchi.searchSport();
-		matchi.chooseSport("Badminton");
+		}
 		
-	}
+// @Test	
+	public void LogOutTest() {
+		MatchiSearch matchi = new MatchiSearch();
+		matchi.openSite("https://beta1.matchi.se/?lang=sv");
+		matchi.logIn();
+		matchi.logOut();
+		matchi.quitSelenium();
+		}
 	
-	
-//	@Test
+	@Test
 	public void MatchiTest() {
 		MatchiSearch matchi = new MatchiSearch();
 		matchi.openSite("https://beta1.matchi.se/?lang=sv");
+		matchi.logIn();		
+		matchi.book();
+		matchi.searchInOutCourt();
+		matchi.chooseInOutCourt("Inomhus");
+		delay(1000);
 		matchi.searchSport();
-		matchi.chooseSportWithOutLogin("Tennis");
-		matchi.searchDay();
-		matchi.chooseDay("24");
-		matchi.searchArea();
-		matchi.chooseArea("Göteborg");
-		delay(2000);
+		matchi.chooseSport("Badminton");
+		matchi.searchAreaOrVenue("Hönö Tennissällskap");
 		matchi.chooseVenue("Hönö Tennissällskap");
-	}			
+		matchi.chooseDayTimeCourt();
+		delay(2000);
+		matchi.payBySwish("");
+		delay(5000);
+		matchi.writeNumber("0123456789");
+		delay(5000);
+	//	matchi.logOut();
+	//	matchi.quitSelenium();
+		}
 	
 	private void delay(int milliseconds)
 	{
@@ -44,10 +53,14 @@ public class MatchiSearchTest {
 		}
 	}
 	
-	@Test
-	public void testOrder() {
-		logInTest();
-		}
+/*	@Test
+	public void testOrder1() {
+		LogInTest();BookCourtTest();PayCourtTest();LogOutTest();
 	}
+*/	
+		
+
+	
+	
 
 }
