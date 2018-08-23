7710711fb460c00fec267599a7c81316fc2dd5c9
mrs-mowgli <38504949+mrs-mowgli@users.noreply.github.com>
2018-08-23 21:20:11 +0200

    Add methods for US 4 & 6
    Add methods for US 4 & 6

--- MATCHi3/MatchiSearch.java
+++ MATCHi3/MatchiSearch.java
@@ -154,6 +154,7 @@ public class MatchiSearch {
 				element.click();
 				element.sendKeys(Keys.TAB);
 				element.clear();
+			//	element.sendKeys(Keys.CONTROL+"a");
 				element.sendKeys(string);
 			}
 			
@@ -166,6 +167,7 @@ public class MatchiSearch {
 				element.click();
 				element.sendKeys(Keys.TAB);
 				element.clear();
+			//	element.sendKeys(Keys.CONTROL+"a");
 				element.sendKeys(string);
 			}
 			
@@ -277,8 +279,118 @@ public class MatchiSearch {
 			}
 			
 			//
-			//  METHODS FOR TESTING PAYMENT BY CREDIT CARD
+			//  METHODS FOR TESTING BOOK PAYMENT BY CREDIT CARD
 			//
+			
+			/**
+			 * MSJ
+			 * fills out the desired date
+			 * @param string is the date given.
+			 */
+			public void chooseDate(String string) {
+				WebElement element = webDriver.findElement(By.xpath("//input[@id='date']"));
+				element.click();
+				element.sendKeys(Keys.CONTROL+"a");
+				element.sendKeys(string);
+			}
+			/**
+			 *MSJ
+			 * once a venue is chosen this method chooses a time.
+			 * @param string determine what court vertically with "2" being top choice and 3 the one below and so on. 
+			 * @param string2 determines courttime horizontally with "1" being the leftmost time and then ascending in numerical order.
+			 */
+			public void chooseCourtTime(String string, String string2) {
+				WebElement element = webDriver.findElement(By.xpath("//tbody//tr["+string+"]//td[2]//table[1]//tbody[1]//tr[1]//td["+string2+"]"));
+				element.click();
+			}
+			/**
+			 * simply confirming confirming after adding the times.
+			 */
+			public void confirmMultCourtTime() {
+				WebElement element = webDriver.findElement(By.xpath("//div[@class='col-sm-5 col-xs-12 no-horizontal-padding text-right']//a[@id='block-book']"));
+				element.click();
+				delay(2000);
+				WebElement element2 = webDriver.findElement(By.xpath("//input[@id='btnSubmit']"));
+				element2.click();
+			}
+			/**
+			 * simply clicking the "boka flera" button.
+			 */
+			public void pickMultCourtTime() {
+				WebElement element = webDriver.findElement(By.xpath("//div[@class='col-sm-5 col-xs-12 no-horizontal-padding text-right']//a[@id='block-book-start']"));
+				element.click();
+			}
+			
+			/**
+			 * Goes through the checkout process and fills out credit card info
+			 * @param string -Is the cardnumber
+			 * @param string2 -Name of cardholder
+			 * @param string3 -fills out the CVC
+			 */
+			public void payCourtByCard(String string, String string2, String string3) {
+				WebElement element = webDriver.findElement(By.xpath("//label[@for='CREDIT_CARD']"));
+				element.click();
+				WebElement element2 = webDriver.findElement(By.xpath("//input[@id='btnSubmit']"));
+				element2.click();
+				WebElement element3 = webDriver.findElement(By.xpath("//input[@placeholder='Kortnummer']"));
+				element3.click();
+				element3.sendKeys(string);
+				WebElement element4 = webDriver.findElement(By.xpath("//input[@placeholder='För- och efternamn']"));
+				element4.click();
+				element4.sendKeys(string2);
+				WebElement element5 = webDriver.findElement(By.xpath("//input[@placeholder='cvc / cid']"));
+				element5.click();
+				element5.sendKeys(string3);
+				WebElement element6 = webDriver.findElement(By.xpath("//option[@value='10']"));
+				element6.click();
+				WebElement element7 = webDriver.findElement(By.xpath("//option[@value='2020']"));
+				element7.click();
+				WebElement element8 = webDriver.findElement(By.xpath("//input[@value='Slutför betalning']"));
+				element8.click();
+
+				
+			}
+			/**
+			 * checks the page for info, atm it checks the top sentence.
+			 * @param expected -expected text when method run like "Tack för din bokning!"
+			 * @return -returns true if expected matches what the page says.
+			 */
+			public boolean assertBooking(String expected) {
+				WebElement element = webDriver.findElement(By.xpath("//*[@id=\"userBookingModal\"]/div[1]/div/div[2]/h1"));
+				String text = element.getText();	
+				return expected.contains(text);
+			}	
+			/**
+			 * unbooks the time to reset teststate
+			 * will rewrite this and unBookAgain to be one and the same
+			 */
+			public void unBook() {
+				WebElement element = webDriver.findElement(By.xpath("//a[@class='btn btn-success']"));
+				element.click();
+				WebElement element2 = webDriver.findElement(By.xpath("//span[@class='badge']"));
+				element2.click();
+				WebElement element3 = webDriver.findElement(By.xpath("//a[@class='userCancelBooking']//div[@class='media-body']"));
+				element3.click();
+				delay(2000);
+				WebElement element4 = webDriver.findElement(By.xpath("//*[@id=\"userBookingModal\"]/div[2]/div/div[3]/a[2]"));
+				element4.click();
+				WebElement element5 = webDriver.findElement(By.xpath("//button[@id='cancelCloseBtn']"));
+				element5.click();
+			}
+			/**
+			 * temporary fix to unBook method.
+			 */
+			public void unBookAgain() {
+				WebElement element2 = webDriver.findElement(By.xpath("//span[@class='badge']"));
+				element2.click();
+				WebElement element3 = webDriver.findElement(By.xpath("//a[@class='userCancelBooking']//div[@class='media-body']"));
+				element3.click();
+				delay(2000);
+				WebElement element4 = webDriver.findElement(By.xpath("//*[@id=\"userBookingModal\"]/div[2]/div/div[3]/a[2]"));
+				element4.click();
+				WebElement element5 = webDriver.findElement(By.xpath("//button[@id='cancelCloseBtn']"));
+				element5.click();
+			}
 		
 			
 			//
@@ -312,6 +424,14 @@ public class MatchiSearch {
 			// METHOD FOR TESTING BOOKING SEVERAL TIMESLOTS AND/OR COURTS
 			// 
 			
+		
+		public void delay(int milliseconds)
+		{
+			try {
+				Thread.sleep(milliseconds);
+			} catch (InterruptedException e) {
+			}
+		}
 			public void quitSelenium() {
 				webDriver.quit();
 				}
